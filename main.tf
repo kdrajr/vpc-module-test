3e20f471ce47878aa7de94198b1e14e3bf98fd7f
Dheeraj <31932986+kdrajr@users.noreply.github.com>
2025-10-25 18:36:50 +0530

    added vpc peering

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ module "vpc" {
   pub_subnet_cidrs = var.pub_subnet_cidrs
   priv_subnet_cidrs = var.priv_subnet_cidrs
   db_subnet_cidrs = var.db_subnet_cidrs
-  is_vpc_peering_required = false
+  is_vpc_peering_required = true
   vpc_tags = var.vpc_tags
 }
 
